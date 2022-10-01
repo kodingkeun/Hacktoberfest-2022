e5a29d13a5f16671c6b930fc8a8619aab7d05739
Akmal Fauzi <akmaledwardfauzi@gmail.com>
2022-10-01 16:35:41 +0700

    feat: add operator arithmetic in dart

---
+++ src/Dart/Akmalfauzi.dart
@@ -0,0 +1,14 @@
+void main() {
+  var a = 5;
+  var b = 10;
+
+  var perkalian = a * b;
+  var pembagian = a / b;
+  var penambahan = a + b;
+  var pengurangan = a - b;
+
+  print("perkalian : ${perkalian}");
+  print("pembagian : ${pembagian}");
+  print("penambahan : ${penambahan}");
+  print("pengurangan : ${pengurangan}");
+}
